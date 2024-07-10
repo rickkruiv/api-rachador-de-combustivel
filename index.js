ee995f5165803e2f1fcf336f7f9ea3a3bec6440a
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2024-07-10 16:09:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const port = 3000;
+
+server.use(cors())
+
+const combustiveis = require('./src/data/combustiveis.json')
+
+server.get('/combustiveis', (req,res) => {
+    return res.json(combustiveis)
+});
+
+server.listen(port, () => {
+    console.log(`API rodadndo em "http://localhost:${port}"`)
+});
